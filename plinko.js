bbf2a33f71da87fb8cda112eb486334415a2521c
Drew <32966255+drewgill89@users.noreply.github.com>
2018-02-20 08:42:33 -0500

    Null Hypothesis
    Implemented null hypothesis testing with alpha = 0.05

--- plinko.js
+++ plinko.js
@@ -5,7 +5,7 @@
 
 
 //array is amount of instances of each value (index)
-var plinkoArray = [0, 2, 3, 2, 2, 2, 0, 1, 1, 1];
+var plinkoArray = [0, 2, 3, 2, 2, 2, 0, 1, 1, 100];
 //finding mean
 function findMean(dataArray){
   var total = 0;
@@ -53,11 +53,13 @@ function findGaussian(dataArray, inMean, inSx){
 		amountArray[0] += dataArray[i];
 	}
 	
-	s1 = (amountArray[3]/amountArray[0]) * 100;
-	s2 = (amountArray[2]/amountArray[0]) * 100;
-	s3 = (amountArray[1]/amountArray[0]) * 100;
+	s1 = (amountArray[3]/amountArray[0]);
+	s2 = (amountArray[2]/amountArray[0]);
+	s3 = (amountArray[1]/amountArray[0]);
 	return [s1,s2,s3];
 }
+
+//returns PROPORTIONS
 //Ideal: 68-95-99.7
 //s1 = proportion within 1 sx, s2 = 2sx, s3 = 3sx
 
@@ -80,10 +82,11 @@ function newBall(dataArray){
 	var mean = findMean(dataArray);
 	var sX = standDev(dataArray, mean);
 	var gaussian = findGaussian(dataArray, mean, sX);
+	var nullHyp = nullHypothesis(zStats(gaussian, total));
 	
 	var standDevMean = (sX/Math.sqrt(total));
 	
-	return [total, mean, sX, standDevMean, gaussian];
+	return [total, mean, sX, standDevMean, gaussian, nullHyp];
   }
   
   
@@ -105,10 +108,10 @@ reject null, conclude Ha, differs from normal curve
 */
 
 //Returns z test statistic of difference from z1 
-  function nullHypothesis(gaus, tot){
-    z1 = (gaus[0] - p)/Math.sqrt(0.2176/tot);
-    z2 = (gaus[1] - p)/Math.sqrt(.0475/tot);
-    z3 = (gaus[2] - p)/Math.sqrt(.002991/tot);
+  function zStats(gaus, tot){
+    z1 = (gaus[0] - 0.68)/Math.sqrt(0.2176/tot);
+    z2 = (gaus[1] - 0.95)/Math.sqrt(.0475/tot);
+    z3 = (gaus[2] - 0.997)/Math.sqrt(.002991/tot);
     
     var curve = [z1, z2, z3];
     
@@ -116,6 +119,20 @@ reject null, conclude Ha, differs from normal curve
   }
   
   
-  
+//if false, fail to reject the null hypothesis (the given sample follows a normal distribution)
+//if true, reject the normal hypothesis (the given sample does not follow a normal distribution)
+  function nullHypothesis(zStat){
+    var rejectNull = [false, false, false];
+    
+    
+    if (Math.abs(zStat[0]) > 1.96)
+      rejectNull[0] = true;
+    if (Math.abs(zStat[1]) > 1.96)
+      rejectNull[1] = true;
+    if (Math.abs(zStat[2]) > 1.96)
+      rejectNull[2] = true;  
+      
+    return rejectNull;
+  }
   
   console.log(newBall(plinkoArray));
